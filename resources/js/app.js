8e3fd1476ac63f90125316eb34086a101a1e23cf
nahidul77 <nahidul.mee@gmail.com>
2023-01-30 01:04:40 +0600

    laravel vue vite project setup

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import './bootstrap';
+
+import { createApp } from 'vue/dist/vue.esm-bundler'
+import PostsIndex from './components/Posts/Index.vue'
+
+const app = createApp({})
+
+app.component('posts-index', PostsIndex)
+app.mount('#app')
