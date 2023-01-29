165147adca52c246eb1eee3a697af8bfca6ca4ca
nahidul77 <nahidul.mee@gmail.com>
2023-01-30 01:24:16 +0600

    tailwind setup

--- resources/js/components/Posts/Index.vue
+++ resources/js/components/Posts/Index.vue
@@ -1,3 +1,33 @@
 <template>
-    Table coming soon
+    <table class="border-collapse border border-slate-400 ...">
+  <thead>
+    <tr>
+      <th class="border border-slate-300 ...">State</th>
+      <th class="border border-slate-300 ...">City</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td class="border border-slate-300 ...">Indiana</td>
+      <td class="border border-slate-300 ...">Indianapolis</td>
+    </tr>
+    <tr>
+      <td class="border border-slate-300 ...">Ohio</td>
+      <td class="border border-slate-300 ...">Columbus</td>
+    </tr>
+    <tr>
+      <td class="border border-slate-300 ...">Michigan</td>
+      <td class="border border-slate-300 ...">Detroit</td>
+    </tr>
+  </tbody>
+</table>
+
+<div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+  <div class="shrink-0">
+  </div>
+  <div>
+    <div class="text-xl font-medium text-black">ChitChat</div>
+    <p class="text-slate-500">You have a new message!</p>
+  </div>
+</div>
 </template>
